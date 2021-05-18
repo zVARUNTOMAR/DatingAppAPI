4243288322e0c375cb840219e41ddd5403eaaaac
Varun Tomar <varuntomar3301@outlook.com>
2021-05-18 13:20:04 +0530

    intial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using DatingAppAPI.Data;
+using DatingAppAPI.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly DataContext _dataContext;
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUser() {
+
+            var users = await _dataContext.Users.ToListAsync();
+
+            return users; 
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DatingAppAPI.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAppAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+
+        } 
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
